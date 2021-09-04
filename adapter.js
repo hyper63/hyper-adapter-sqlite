eaa6a13c12c97b1a8e58eec28002d6f30b1e4e07
Tom Wilson <twilson63@gmail.com>
2021-09-04 23:25:23 +0000

    got basic createDoc working

--- adapter.js
+++ adapter.js
@@ -1,35 +1,42 @@
 import { DB } from './deps.js'
 
 const createTable = name => `
-CREATE TABLE IF NOT EXISTS stores (
+CREATE TABLE IF NOT EXISTS ${name} (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   key TEXT,
   value TEXT,
   ttl INTEGER
 )
 `
+const insertDoc = (table) => `
+insert into ${table} (key,value) values (?, ?)`
 export default (db) => {
 
   const createStore = (name) => {
     try {
+      console.log(createTable(name))
       db.query(createTable(name))
       return Promise.resolve(({ ok: true }))
     } catch (e) {
+      console.log(e)
       return Promise.reject({ ok: false, status: 500, msg: 'Could not create store!' })
     }
   }
 
   const createDoc = ({ store, key, value, ttl }) => {
+    console.log(insertDoc(store))
     try {
-      db.query(insertDoc(key, JSON.stringify(value), ttl))
+      db.query(insertDoc(store), [key, JSON.stringify(value)]) //ttl
       return Promise.resolve({ ok: true })
     } catch (e) {
-      return Promise.resolve({ ok: false, status: 400 })
+      console.log(e)
+      return Promise.resolve({ ok: false, status: 409 })
     }
   }
 
   const deleteDoc = async ({ store, key }) => {
     //const res = await stores[store].del(key)
+    db.query(`delete from ${store} where key = ?`, [key])
     return ({ ok: true })
   }
 
--- adapter_test.js
+++ adapter_test.js
@@ -1,7 +1,8 @@
+import { DB } from './deps.js'
 import adapter from "./adapter.js";
 import { assert } from "./dev_deps.js";
 
-const cache = adapter({ dir: "/tmp" });
+const cache = adapter(new DB(`./test.db`));
 const test = Deno.test;
 
 test("create cache store", async () => {
@@ -11,6 +12,8 @@ test("create cache store", async () => {
     key: "1",
     value: { type: "movie", title: "Ghostbusters" },
   });
+  console.log(res)
   assert(res.ok);
-  await cache.deleteDoc("1");
+  await cache.deleteDoc({ store: 'test', key: '1' });
+
 });
