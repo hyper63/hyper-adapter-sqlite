b03c97de96a4eae127482e8215b5306881eb61b9
Tom Wilson <twilson63@gmail.com>
2021-09-07 16:09:57 +0000

    close db when program is unloaded

--- mod.js
+++ mod.js
@@ -2,12 +2,19 @@ import adapter from "./adapter.js";
 import PORT_NAME from "./port_name.js";
 import { DB } from "./deps.js";
 
+let db = null;
+
 export default (config) => ({
   id: "sqlite",
   port: PORT_NAME,
   load: () => {
     const dir = config.dir || ".";
-    return new DB(`${dir}/cache.db`); // load env
+    db = new DB(`${dir}/cache.db`);
+    return db; // load env
   },
   link: (env) => (_) => adapter(env), // link adapter
+});
+
+window.addEventListener("unload", () => {
+  if (db) db.close();
 });
