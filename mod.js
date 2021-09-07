26886d0360df146bb5e4f1c2dfb012cceaccecdb
Tom Wilson <twilson63@gmail.com>
2021-09-07 17:21:06 +0000

    added unload sqlite db handler

--- mod.js
+++ mod.js
@@ -2,19 +2,24 @@ import adapter from "./adapter.js";
 import PORT_NAME from "./port_name.js";
 import { DB } from "./deps.js";
 
-let db = null;
-
 export default (config) => ({
   id: "sqlite",
   port: PORT_NAME,
   load: () => {
     const dir = config.dir || ".";
-    db = new DB(`${dir}/cache.db`);
+    let db = new DB(`${dir}/cache.db`);
+
+    window.addEventListener("unload", () => {
+      if (db) {
+        try {
+          db.close(true);
+        } catch (e) { console.log(e) }
+      }
+    });
+
     return db; // load env
   },
   link: (env) => (_) => adapter(env), // link adapter
 });
 
-window.addEventListener("unload", () => {
-  if (db) db.close();
-});
+
