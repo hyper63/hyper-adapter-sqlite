e3347ed9899d31ef9340628fa0f25577cf819271
Tyler Hall <tyler@hyper.io>
2021-09-23 16:37:55 -0400

    fix(escape): escape table name in sql queries #6

--- adapter.js
+++ adapter.js
@@ -8,8 +8,12 @@ const {
   zipObj,
 } = R;
 
+function quote(str) {
+  return `"${str}"`;
+}
+
 const createTable = (name) => `
-CREATE TABLE IF NOT EXISTS ${name} (
+CREATE TABLE IF NOT EXISTS ${quote(name)} (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   key TEXT,
   value TEXT,
@@ -18,7 +22,7 @@ CREATE TABLE IF NOT EXISTS ${name} (
 )
 `;
 const insertDoc = (table) => `
-insert into ${table} (key,value,ttl,timestmp) values (?, ?, ?, ?)`;
+insert into ${quote(table)} (key,value,ttl,timestmp) values (?, ?, ?, ?)`;
 
 export default (db) => {
   const createStore = (name) => {
@@ -36,7 +40,9 @@ export default (db) => {
 
   const createDoc = ({ store, key, value, ttl }) => {
     try {
-      const res = db.query(`select key from ${store} where key = ?`, [key]);
+      const res = db.query(`select key from ${quote(store)} where key = ?`, [
+        key,
+      ]);
       if (res.length > 0) {
         return Promise.reject({
           ok: false,
@@ -59,7 +65,7 @@ export default (db) => {
 
   const deleteDoc = ({ store, key }) => {
     //const res = await stores[store].del(key)
-    db.query(`delete from ${store} where key = ?`, [key]);
+    db.query(`delete from ${quote(store)} where key = ?`, [key]);
     return Promise.resolve({ ok: true });
   };
 
@@ -83,19 +89,21 @@ export default (db) => {
   const getDoc = ({ store, key }) => {
     try {
       const res = db.query(
-        `select id, key, value, ttl, timestmp from ${store} where key = ?`,
+        `select id, key, value, ttl, timestmp from ${
+          quote(store)
+        } where key = ?`,
         [
           key,
         ],
       );
       const doc = xDoc(res);
       if (doc.ttl > 0 && expired(doc.ttl, doc.timestmp)) {
-        db.query(`delete from ${store} where id = ?`, [doc.id]);
+        db.query(`delete from ${quote(store)} where id = ?`, [doc.id]);
         return Promise.reject({ ok: false, status: 404, msg: "ttl expired!" });
       }
       // update timestmp
       db.query(
-        `update ${store} set timestmp = ? where id = ?`,
+        `update ${quote(store)} set timestmp = ? where id = ?`,
         new Date().toISOString(),
         doc.id,
       );
@@ -112,12 +120,17 @@ export default (db) => {
 
   const updateDoc = ({ store, key, value, ttl }) => {
     try {
-      const res = db.query(`select id, value from ${store} where key = ?`, [
-        key,
-      ]);
+      const res = db.query(
+        `select id, value from ${quote(store)} where key = ?`,
+        [
+          key,
+        ],
+      );
       if (res.length === 0) {
         db.query(
-          `insert into ${store} (key, value, ttl, timestmp) values (?, ?, ?, ?)`,
+          `insert into ${
+            quote(store)
+          } (key, value, ttl, timestmp) values (?, ?, ?, ?)`,
           [
             key,
             JSON.stringify(value),
@@ -131,7 +144,9 @@ export default (db) => {
       const cur = JSON.parse(res[0][1]);
       value = { ...cur, ...value };
       const _ = db.query(
-        `update ${store} set value = ?, ttl = ?, timestmp = ? where id = ?`,
+        `update ${
+          quote(store)
+        } set value = ?, ttl = ?, timestmp = ? where id = ?`,
         [JSON.stringify(value), ttl, new Date().toISOString(), id],
       );
       return Promise.resolve({ ok: true });
@@ -147,9 +162,12 @@ export default (db) => {
 
   const listDocs = ({ store, pattern }) => {
     try {
-      const res = db.query(`select key, value from ${store} where key like ?`, [
-        pattern.replace("*", "%"),
-      ]);
+      const res = db.query(
+        `select key, value from ${quote(store)} where key like ?`,
+        [
+          pattern.replace("*", "%"),
+        ],
+      );
       const toObject = ([k, v]) => ({ key: k, value: JSON.parse(v) });
       return Promise.resolve({
         ok: true,
@@ -170,7 +188,7 @@ export default (db) => {
 
   const destroyStore = (name) => {
     try {
-      const res = db.query(`drop table ${name}`);
+      const res = db.query(`drop table ${quote(name)}`);
       console.log(res);
       return Promise.resolve({ ok: true });
     } catch (_e) {
