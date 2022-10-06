a836f5b484347c159ee502f1aff6d27a2d87e970
Tyler Hall <tyler@hyper.io>
2022-10-06 19:14:36 +0000

    chore: bump deps

--- dev_deps.js
+++ dev_deps.js
@@ -1,9 +1,9 @@
 // Schema parsing deps
-export { default as validateFactorySchema } from "https://x.nest.land/hyper@3.2.0/utils/plugin-schema.js";
+export { default as validateFactorySchema } from "https://x.nest.land/hyper@3.3.0/utils/plugin-schema.js";
 export { cache as validateCacheAdapterSchema } from "https://x.nest.land/hyper-port-cache@1.2.0/mod.js";
 
 // std lib deps
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.135.0/testing/asserts.ts";
+} from "https://deno.land/std@0.159.0/testing/asserts.ts";
