9e2c3af092e58e8782b22712ddff457b3b56bbbe
Tyler Hall <tyler@hyper.io>
2022-11-16 21:41:18 +0000

    chore: always enable deno. bu p deps

--- deps.js
+++ deps.js
@@ -6,7 +6,7 @@ export {
   isHyperErr,
 } from "https://x.nest.land/hyper-utils@0.1.0/hyper-err.js";
 
-export { DB } from "https://deno.land/x/sqlite@v3.5.0/mod.ts";
+export { DB } from "https://deno.land/x/sqlite@v3.7.0/mod.ts";
 export { default as addMilliseconds } from "https://deno.land/x/date_fns@v2.22.1/addMilliseconds/index.ts";
 export { default as isAfter } from "https://deno.land/x/date_fns@v2.22.1/isAfter/index.ts";
 export { default as parseISO } from "https://deno.land/x/date_fns@v2.22.1/parseISO/index.js";
--- dev_deps.js
+++ dev_deps.js
@@ -6,4 +6,4 @@ export { cache as validateCacheAdapterSchema } from "https://x.nest.land/hyper-p
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.159.0/testing/asserts.ts";
+} from "https://deno.land/std@0.164.0/testing/asserts.ts";
