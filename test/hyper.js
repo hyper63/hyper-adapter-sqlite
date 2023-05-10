a0335b6e2a4e917dc6ebbfcf29e20caffea5b372
Tyler Hall <tyler@hyper.io>
2023-05-10 21:23:05 +0000

    test: update harness to use latest deps. Rename test files #17

--- test/hyper.js
+++ test/hyper.js
@@ -1,12 +1,12 @@
 // Harness deps
-import { default as appOpine } from 'https://x.nest.land/hyper-app-opine@2.2.0/mod.js'
-import { default as core } from 'https://x.nest.land/hyper@3.3.0/mod.js'
+import { default as appExpress } from 'https://x.nest.land/hyper-app-express@1.0.0/mod.ts'
+import { default as core } from 'https://x.nest.land/hyper@3.4.2/mod.js'
 
 import myAdapter from '../mod.js'
 import PORT_NAME from '../port_name.js'
 
 const hyperConfig = {
-  app: appOpine,
+  app: appExpress,
   adapters: [
     { port: PORT_NAME, plugins: [myAdapter({ dir: '/tmp' })] },
   ],
