ae3030e445776f36e15b9862c1a3a3dd980622e9
Tyler Hall <tyler@hyper.io>
2023-06-05 09:14:57 -0400

    test: update test suite to latest version

--- test/hyper.js
+++ test/hyper.js
@@ -1,6 +1,6 @@
 // Harness deps
-import { default as appExpress } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.0.2/packages/app-express/mod.ts'
-import { default as core } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.0.1/packages/core/mod.ts'
+import { default as appExpress } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.1.0/packages/app-express/mod.ts'
+import { default as core } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.1.0/packages/core/mod.ts'
 
 import myAdapter from '../mod.js'
 import PORT_NAME from '../port_name.js'
